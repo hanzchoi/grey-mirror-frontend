395678822840c1cadf4af0545508a6e62ea8d464
github username <personabe1984@gmail.com>
2019-02-10 20:57:28 -0500

    added Rank section to the app

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'John Doe, Your current rank is...'}
+      </div>
+      <div className='white f1'>
+        {'Over 9000'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
