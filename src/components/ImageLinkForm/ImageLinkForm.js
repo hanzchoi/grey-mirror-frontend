e1c3e0a962244dfafe13eded037dc079ec9149f5
github username <personabe1984@gmail.com>
2019-02-10 19:41:45 -0500

    Added a button and styled it with tachyon

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ImageLinkForm = () =>{
   return (
-    <div></div>
-  )
+    <div>
+      <p className='f3'>
+        {'This app will detect faces in your pictures. Try it out'}
+      </p>
+      <div>
+        <input className='f4 p2 w-70 center' type='text'/>
+        <button className='w-30 grow f4 link pv2 dib white bg-light-purple'>Detect</button>
+      </div>
+    </div>
+  );
 }
 export default ImageLinkForm;
