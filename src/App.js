83851ad8aa17898d3d7419a1caf60e45668322a3
github username <personabe1984@gmail.com>
2019-02-11 19:01:58 -0500

    integrating Particles and playing around with it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
+const particlesOption = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles' params={particlesOption} />
         <Navigation />
         <Logo />
         <Rank />
