fcd1f810faa1affa90a40c43b9f937b9fd98e172
github username <personabe1984@gmail.com>
2019-02-17 18:59:55 -0500

    testing out the clarifi api. Running into some issue but wil be resolvved on the next commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,15 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('test')
+    console.log('test');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render() {
