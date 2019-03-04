85378dbd1119cf6ee3f8f19e2d43a847230792a8
github username <personabe1984@gmail.com>
2019-03-04 18:31:23 -0500

    cleaned up the onButtonSubmit to match es6

--- src/App.js
+++ src/App.js
@@ -34,6 +34,9 @@ class App extends Component {
       box: {}
     }
   }
+  calculateFaceLocation = (data) => {
+
+  }
 
   onInputChange = (event) => {
     this.setState({input: event.target.value})
@@ -44,18 +47,12 @@ class App extends Component {
     this.setState({imageURL: this.state.input})
     app.models
       .predict(
-      Clarifai.FACE_DETECT_MODEL,
-        this.state.input
-      )
-      .then(function(response) {
-          // do something with responseconsole.log(response);
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-          },
-          function(err) {// there was an error
-          }
-      );
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.calculateFaceLocation(response))
+      .catch(error => console.log(error))
   }
-
+  //console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
   render() {
     return (
       <div className="App">
